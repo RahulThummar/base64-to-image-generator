624b1e1486c5b22842dc42b5b12ca0e60df5855b
RahulThummar <rthummar788@rku.ac.in>
2024-05-25 17:39:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { createContext, useState } from "react";
+import InputForm from "./components/InputForm"; 
+
+// Create context
+export const InputContext = createContext();
+
+function App() {
+  const [inputValue, setInputValue] = useState({
+    url: "",
+    color: "",
+  });
+  const [response, setResponse] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getQrCode = async () => {
+    try {
+      setLoading(true);
+      setResponse({ text: inputValue.url, color: inputValue.color });
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const value = {
+    inputValue,
+    setInputValue,
+    getQrCode,
+    response,
+    loading,
+    error,
+  };
+
+  return (
+    // <div>
+    //   <h2 className="mt-5">QR code generator</h2>
+    //   <div className="container">
+    //     <div className="main-content">
+    //       <div className="md:grid md:grid-cols-5">
+            <InputContext.Provider value={value}>
+              <InputForm />
+              {/* <QrCode /> */}
+            </InputContext.Provider>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+  );
+}
+
+export default App;
---
+++ src/components/InputForm.js
@@ -0,0 +1,72 @@
+import { useContext, useEffect, useState } from "react";
+import { InputContext } from "../App";
+import { ChromePicker } from "react-color";
+
+const InputForm = () => {
+  const { getQrCode, inputValue, setInputValue } = useContext(InputContext);
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    setInputValue({ ...inputValue });
+  }, []);
+
+  const handleOnChange = (e) => {
+    setInputValue({ ...inputValue, url: e.target.value });
+  };
+
+  const handleSubmit = () => getQrCode();
+
+  const handleDownloadImage = () => {
+    if (!inputValue.url) return;
+
+    const link = document.createElement("a");
+    link.href = inputValue.url;
+    link.download = "downloaded_image.png"; // Change the file name if needed
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  useEffect(() => {
+    handleSubmit();
+  }, [inputValue.url, inputValue.color]);
+
+  // Update the imageUrl whenever inputValue.url changes
+  useEffect(() => {
+    setImageUrl(inputValue.url);
+  }, [inputValue.url]);
+
+  return (
+    <div className="main-container">
+      <div className="content-wrapper">
+        <h2 className="mt-5">Base64 to Image generator</h2>
+        <div className="input-container">
+          <textarea
+            rows={5}
+            type="url"
+            className="w-full border-2 py-1 px-3 text-gray-700 rounded-sm"
+            placeholder="Enter base64 url here..."
+            value={inputValue.url}
+            onChange={handleOnChange}
+          />
+          <button
+            disabled={!inputValue.url}
+            onClick={handleDownloadImage}
+            className="bg-blue-400 px-4 py-2 text-white rounded-sm ml-2 hover:bg-blue-500 disabled:bg-gray-300"
+          >
+            Download Image
+          </button>
+        </div>
+        <div className="container mt-5">
+          <div className="main-content">
+            {imageUrl && (
+              <img src={imageUrl} alt="Preview" className="preview-image" />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InputForm;
