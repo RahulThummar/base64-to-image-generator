88a671d272aee8e45be35d2ef18a9a430d9f7e50
RahulThummar <rthummar788@rku.ac.in>
2024-05-25 18:57:30 +0530

    first commit

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,6 +1,5 @@
 import { useContext, useEffect, useState } from "react";
 import { InputContext } from "../App";
-import { ChromePicker } from "react-color";
 
 const InputForm = () => {
   const { getQrCode, inputValue, setInputValue } = useContext(InputContext);
@@ -31,7 +30,6 @@ const InputForm = () => {
     handleSubmit();
   }, [inputValue.url, inputValue.color]);
 
-  // Update the imageUrl whenever inputValue.url changes
   useEffect(() => {
     setImageUrl(inputValue.url);
   }, [inputValue.url]);
@@ -39,28 +37,29 @@ const InputForm = () => {
   return (
     <div className="main-container">
       <div className="content-wrapper">
-        <h2 className="mt-5">Base64 to Image generator</h2>
+        <h2 className="mt-5">Base64 to Image converter</h2>
         <div className="input-container mt-3">
           <textarea
             rows={7}
             type="url"
-            className="w-full border-2 py-1 px-3 text-gray-700 rounded-sm"
+            className="w-full border-2 py-1 px-3 text-gray-700 rounded-sm text-area"
             placeholder="Enter base64 url here..."
             value={inputValue.url}
             onChange={handleOnChange}
           />
-          <button
-            disabled={!inputValue.url}
-            onClick={handleDownloadImage}
-            className="bg-blue-400 px-4 py-2 text-white rounded-sm ml-2 hover:bg-blue-500 disabled:bg-gray-300"
-          >
-            Download Image
-          </button>
         </div>
         <div className="container mt-5">
-          <div className="main-content">
+          <div className="main-content relative">
             {imageUrl && (
-              <img src={imageUrl} alt="Preview" className="preview-image" />
+              <div className="image-container relative group">
+                <img src={imageUrl} alt="Preview" className="preview-image" />
+                <button
+                  onClick={handleDownloadImage}
+                  className="download-button absolute top-0 right-0 bg-blue-400 px-4 py-2 text-white rounded-sm hover:bg-blue-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                >
+                  Download Image
+                </button>
+              </div>
             )}
           </div>
         </div>
